f8ab1be034b055906f5e11771e89c6b3830f1dee
Danil Bikmetov <bicdan22@qavm-56efe012.qemu>
2023-05-19 20:41:15 +0300

    add recommend history

--- core/recommender.py
+++ core/recommender.py
@@ -5,4 +5,11 @@ def recommend(USER_DICT):
     places = utils.get_places('PlacesDatabase/food_places.csv')
     lon, lat = USER_DICT['lon'], USER_DICT['lat']
     nearest_places = utils.get_nearest(places, (lon, lat), 5)
+
+    if 'recommend_history' not in USER_DICT:
+        USER_DICT['recommend_history'] = set()
+    for place in nearest_places:
+        USER_DICT['recommend_history'].add(place)
+    print(USER_DICT['recommend_history'])
+
     return nearest_places
