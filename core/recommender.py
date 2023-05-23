544bf93463267cb66f5991722493d1b7a117178d
Danil Bikmetov <bicdan22@qavm-56efe012.qemu>
2023-05-23 15:32:54 +0300

    new candidates logic

--- core/recommender.py
+++ core/recommender.py
@@ -1,7 +1,73 @@
 import pandas as pd
 from abc import ABC, abstractmethod
+from collections import defaultdict
 from .utils import ItemType, Item, RecommendItem
 from .utils import validate_point, get_nearest, stream_blender_diego
+
+
+class CandidatesHolder:
+
+    """
+    Хранилище кандидатов для рекомендации.
+
+    В класс можно передать уже созданный словарь type_to_candidates,
+    либо оставить поле пустым, после чего будет автоматически создан
+    словарь type_to_candidates для всех доступных на текущий момент
+    видов рекомендаций.
+
+    Метод update обновит информацию о всех доступных в базе данных кандидатах,
+    то есть перезапишет их для дальнейшего использования роекомендером.
+    """
+
+    def __init__(self, type_to_candidates=None):
+        if type_to_candidates is not None:
+            self.type_to_candidates = type_to_candidates
+        else:
+            self.type_to_candidates = {
+                ItemType.FOOD: defaultdict(Item),
+                ItemType.SHOP: defaultdict(Item)
+            }
+
+    def get_candidates_by_type(self, item_type):
+        return self.type_to_candidates[item_type]
+
+    def update(self, food_path, shop_path):
+        self.type_to_candidates[ItemType.FOOD] =\
+            self.read_food_candidates(food_path)
+        self.type_to_candidates[ItemType.SHOP] =\
+            self.read_shop_candidates(shop_path)
+
+    def read_food_candidates(self, food_path):
+        df_food = pd.read_csv(food_path)
+        food_candidates = {}
+        for row in df_food.itertuples():
+            lon, lat = row.Longitude_WGS84_en, row.Latitude_WGS84_en
+            if validate_point((lon, lat)):
+                item = Item(
+                    ItemType.FOOD,
+                    row.Name_en,
+                    row.Address_en,
+                    lon,
+                    lat)
+                food_candidates[item.item_id] = item
+        return food_candidates
+
+    def read_shop_candidates(self, shop_path):
+        df_shop = pd.read_csv(shop_path)
+        shop_candidates = {}
+        for row in df_shop.itertuples():
+            geo_data = row.geoData
+            coords = geo_data.split(']')[0].split('[')[1].split(',')
+            lon, lat = float(coords[0]), float(coords[1])
+            if validate_point((lon, lat)):
+                item = Item(
+                    ItemType.SHOP,
+                    row.Наименование,
+                    row.Адрес,
+                    lon,
+                    lat)
+                shop_candidates[item.item_id] = item
+        return shop_candidates
 
 
 class Recommender(ABC):
@@ -13,8 +79,21 @@ class Recommender(ABC):
     играющие ключевую роль в рекомендациях.
     """
 
+    def __init__(self, item_type, candidates_holder):
+        """
+        Хранит словарь кандидатов.
+
+        Словарь кандидатов должен быть полем в CandidatesHolder.
+        Ключом словаря является item_id, значением - item.
+        Задаётся в моменте создания рекомендера.
+        Обновляется в CandidatesHolder.
+        """
+        self.item_type = item_type
+        self.candidates_holder = candidates_holder
+
     @abstractmethod
-    def get_light_recommender_items(self, USER_INFO, items, coords, limit):
+    def get_light_recommender_items(self, USER_INFO, candidates,
+                                    coords, limit):
         """
         Лёгкий рекомендер.
 
@@ -33,8 +112,8 @@ class Recommender(ABC):
         if 'recommend_history' not in USER_INFO:
             USER_INFO['recommend_history'] = set()
 
-    def stream_blender(self, USER_INFO, recommended_items, blender_limit=5,
-                       mode='Diego', temperature=5):
+    def stream_blender(self, USER_INFO, recommended_items,
+                       blender_limit=5, mode='Diego', temperature=5):
         """
         Возвращает финальную пачку рекомендаций с вертикали.
 
@@ -72,9 +151,11 @@ class Recommender(ABC):
         self.before_recommend(USER_INFO)
 
         lon, lat = USER_INFO['lon'], USER_INFO['lat']
+        item_type = self.item_type
+        candidates = self.candidates_holder.get_candidates_by_type(item_type)
         recommended_items = self.get_light_recommender_items(
             USER_INFO,
-            self.candidates,
+            candidates,
             (lon, lat),
             recommend_limit)
 
@@ -89,35 +170,20 @@ class Recommender(ABC):
         return stream_items
 
     def add_rating(self, *, item_id=None, rating_good=True):
-        for idx, cand in enumerate(self.candidates):
-            if cand.item_id == item_id:
-                self.candidates[idx].add_rating(1 * rating_good)
-                print(self.candidates[idx].avg_rating)
+        for candidate_item_id in self.candidates:
+            if candidate_item_id == item_id:
+                self.candidates[candidate_item_id].add_rating(1 * rating_good)
                 return
 
 
 class FoodRecommender(Recommender):
+
     """Рекомендер ресторанов."""
 
-    def __init__(self):
-        """Сохраняет список всех ресторанов."""
-        df_food = pd.read_csv('PlacesDatabase/food_places.csv')
-        candidates = []
-        for row in df_food.itertuples():
-            lon, lat = row.Longitude_WGS84_en, row.Latitude_WGS84_en
-            if validate_point((lon, lat)):
-                item = Item(
-                    ItemType.FOOD,
-                    row.Name_en,
-                    row.Address_en,
-                    lon,
-                    lat)
-                candidates.append(item)
-        self.candidates = candidates
-
-    def get_light_recommender_items(self, USER_INFO, places, coords, limit):
+    def get_light_recommender_items(self, USER_INFO, candidates,
+                                    coords, limit):
         """Возвращает limit ближайших ресторанов."""
-        items_with_dist = get_nearest(USER_INFO, places, coords, limit)
+        items_with_dist = get_nearest(USER_INFO, candidates, coords, limit)
         recommended_items = []
 
         for (item, dist) in items_with_dist:
