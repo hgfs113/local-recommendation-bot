1abb6ec796abcfb07b049b8fd21bb9a6ef147ed8
darkuro <temnoosha@gmail.com>
2023-06-14 20:22:20 +0300

    add test task to dodo

--- dodo.py
+++ dodo.py
@@ -1,10 +1,23 @@
+import glob
+
 def task_gitclean():
     return {
             'actions': ['git clean -Xdf'],
            }
 
+
 def task_html():
     return {
             'actions': ['sphinx-build doc _build'],
+            'file_dep': glob.glob("*.py"),
+            'targets': ['_build/index.html'],
            }
- 
+
+
+def task_test():
+    return {
+            'actions': ['pytest core/test_ut.py'],
+            'file_dep': glob.glob("*.py"),
+            'clean': True,
+           }
+
