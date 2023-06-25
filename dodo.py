7b1ebec6b7f95c4ace942e0bfc48c9f003b06fa0
EvgSumina <eugenia.sumina@yandex.ru>
2023-06-25 19:22:34 +0300

    Trying move to src

--- dodo.py
+++ dodo.py
@@ -1,5 +1,28 @@
 import glob
 from doit.task import clean_targets
+from doit.tools import create_folder
+
+
+# def task_pot():
+#     c1 = 'pybabel extract --input-dirs=. --output-file=messages.pot'
+#     return {'actions': [c1],
+#             'file_dep': glob.glob('*.py'),
+#             'targets': ['messages.pot'], }    
+
+
+# def task_locale():
+#     c1 = 'pybabel update -D geoguesser_bot -d src/locale -i messages.pot'
+#     return {'actions': [c1],
+#             'file_dep': ['messages.pot'],
+#             'targets': ['src/locale/en/LC_MESSAGES/messages.po'], }
+
+
+# def task_mo():
+#     c1 = 'pybabel compile -D geoguesser -l en -i'
+#     c2 = ' locale/en/LC_MESSAGES/messages.po -d .'
+#     return {'actions': [c1 + c2],
+#             'file_dep': ['locale/en/LC_MESSAGES/messages.po'],
+#             'targets': ['locale/en/LC_MESSAGES/messages.mo'], }
 
 
 def task_gitclean():
@@ -35,4 +58,5 @@ def task_wheel():
     return {
             'actions': ['pyproject-build -w'],
             'file_dep': glob.glob("*.py"),
+            'task_dep': ['mo'], 
            }
